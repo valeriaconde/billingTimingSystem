5519ea76b3d64659225c8a47a369291b5885bfc0
EliasMera <a01280762@itesm.mx>
2020-04-18 20:35:59 -0700

    Form state saved

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import { ListGroup, Container, Row, Col, Form, Button, Modal } from 'react-bootstrap';
 import { AuthUserContext, withAuthorization } from './Auth';
 
+const INITIAL_STATE = {
+    denomination: '',
+    address: '',
+    rfc: '',
+    contact: '',
+    email: '',
+    phone: '',
+    website: '',
+    yearSince: '',
+    iva: false,
+    validated: false,
+    edit: false,
+    showModalCliente: false,
+    error: null
+};
+
 class Clientes extends Component {
     constructor(props) {
         super(props);
-        this.state = { edit: false, showModalCliente: false };
+        this.state = { ...INITIAL_STATE };
 
         this.handleCloseCliente = this.handleCloseCliente.bind(this);
         this.handleShowCliente = this.handleShowCliente.bind(this);
+        this.handleNewClient = this.handleNewClient.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleNewClient(event) {
+
+    }
+
+    handleOnChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     handleShowCliente(e) {
@@ -17,6 +43,122 @@ class Clientes extends Component {
 
     handleCloseCliente() {
         this.setState({ showModalCliente: false });
+    }
+
+    renderModal() {
+        const { denomination, address, rfc, contact, email,
+                phone, website, yearSince } = this.state;
+        return(
+            <Modal show={this.state.showModalCliente} onHide={this.handleCloseCliente}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Nuevo cliente</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Denominación
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="denomination" onChange={this.handleOnChange} value={denomination} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Domicilio
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="address" onChange={this.handleOnChange} value={address} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                RFC
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="rfc" onChange={this.handleOnChange} value={rfc} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Contacto
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="contact" onChange={this.handleOnChange} value={contact} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Correo
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="email" onChange={this.handleOnChange} value={email} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Teléfono
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="phone" onChange={this.handleOnChange} value={phone} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Website
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="website" onChange={this.handleOnChange} value={website} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm="3">
+                                Cliente desde
+                            </Form.Label>
+                            <Col sm="5">
+                                <Form.Control name="yearSince" onChange={this.handleOnChange} value={yearSince} as="textarea" rows="1" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} >
+                            <Form.Label column sm="3"> IVA </Form.Label>
+                            <Col sm="5">
+                                <div>
+                                <Form.Check
+                                    type="radio"
+                                    label="SI"
+                                    name="yesIVA"
+                                    id="yesIVA"
+                                    defaultChecked
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="NO"
+                                    name="noIVA"
+                                    id="noIVA"
+                                />
+                                </div>
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleCloseCliente}>
+                        Cancelar
+                    </Button>
+                    <Button className="legem-primary" onClick={this.handleCloseCliente}>
+                        Guardar cliente
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
     }
     
     render() {
@@ -28,114 +170,9 @@ class Clientes extends Component {
                             <Row>
                                 <Col sm={4}>
                                     <Button variant="success" size="lg" block onClick={this.handleShowCliente}> Nuevo Cliente </Button>
-                                    
-                                    {/* MODAL */}
-                                    <Modal show={this.state.showModalCliente} onHide={this.handleCloseCliente}>
-                                            <Modal.Header closeButton>
-                                                <Modal.Title>Nuevo cliente</Modal.Title>
-                                            </Modal.Header>
-                                            <Modal.Body>
-                                                <Form>
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Denominación
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Domicilio
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            RFC
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Contacto
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Correo
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Teléfono
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Website
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row}>
-                                                        <Form.Label column sm="3">
-                                                            Cliente desde
-                                                        </Form.Label>
-                                                        <Col sm="5">
-                                                            <Form.Control as="textarea" rows="1" />
-                                                        </Col>
-                                                    </Form.Group>
-        
-                                                    <Form.Group as={Row} >
-                                                        <Form.Label column sm="3"> IVA </Form.Label>
-                                                        <Col sm="5">
-                                                            <div>
-                                                                {['radio'].map(type => (
-                                                                    <div key={`inline-${type}`} className="mb-3">
-                                                                        <Form.Check inline label="SI" type={type} id={`inline-${type}-1`} />
-                                                                        <Form.Check inline label="NO" type={type} id={`inline-${type}-1`} />
-                                                                    </div>
-                                                                ))}
-                                                            </div>
-                                                        </Col>
-                                                    </Form.Group>
-                                                </Form>
-                                            </Modal.Body>
-                                            <Modal.Footer>
-                                                <Button variant="secondary" onClick={this.handleCloseCliente}>
-                                                    Cancelar
-                                                </Button>
-                                                <Button className="legem-primary" onClick={this.handleCloseCliente}>
-                                                    Guardar cliente
-                                                </Button>
-                                            </Modal.Footer>
-                                        </Modal>
-                                    
-        
-        
-        
+
+                                    {this.renderModal()}
+
                                     <ListGroup as="ul" className="">
                                         <ListGroup.Item as="li" > Cliente 1  </ListGroup.Item>
                                         <ListGroup.Item as="li" active className="legem-primary" >Cliente 2</ListGroup.Item>
