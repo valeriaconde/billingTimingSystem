7995692fe8f0a3635def5fde9d57eebabf9b11fe
Valeria Conde <valeriaconde.96@gmail.com>
2020-08-02 21:14:33 -0700

    not yet finished tho

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {  } from 'react-bootstrap';
+import Select from 'react-select';
+import { Form, Row, Col, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import { AuthUserContext, withAuthorization } from './Auth';
 
 class billing extends Component {
@@ -9,11 +10,52 @@ class billing extends Component {
     render() {
         return (
             <AuthUserContext.Consumer>
-            {authUser =>
-                <div>
+                {authUser =>
+                    <div>
+                        <h4 className="blueLetters topMargin leftMargin"> New notice </h4>
 
-                </div>
-            }
+                        {/* CHOOSE CLIENT */}
+                        <Form className="leftMargin topMargin">
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="3"> Client </Form.Label>
+                                <Col sm="7">
+                                    <Select />
+                                </Col>
+                            </Form.Group>
+
+                            {/* CHOOSE PROJECTS */}
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="3"> Projects: </Form.Label>
+                            </Form.Group>
+
+                            <Form.Group className="leftMargin">
+                                <div key="checbox" className="mb-3">
+                                    <Form.Check type="checkbox" id="checkbox" label="checkbox" />
+                                </div>
+                            </Form.Group>
+
+
+                            {/* CHOOSE LANGUAGE OF NOTICE */}
+                            <Form.Group as={Row} className="topMargin">
+                                <Form.Label column sm="3"> Language of notice </Form.Label>
+                                <Col sm="7">
+                                    <ButtonToolbar className="justify-content-between ">
+                                        <ButtonGroup toggle>
+                                            <Button variant="light">EN</Button>
+                                            <Button variant="light">ES</Button>
+                                        </ButtonGroup>
+                                    </ButtonToolbar>
+                                </Col>
+                            </Form.Group>
+
+                            <Button className="legem-primary" type="submit"> Generate charge notice </Button>
+                            <Button className="legem-primary" type="submit"> Download </Button>
+
+                        </Form>
+
+
+                    </div>
+                }
             </AuthUserContext.Consumer>
         );
     }
